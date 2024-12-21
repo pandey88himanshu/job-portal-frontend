e78adcf8a5727295c141149ababa7d28aff7dea7
pandey88himanshu <pandey88himanshu@gmail.com>
2024-12-21 11:51:15 +0530

    backend added

--- src/components/utils/constant.js
+++ src/components/utils/constant.js
@@ -1,8 +1,8 @@
 export const USER_API_END_POINT =
-  "https://job-portal-rho-hazel.vercel.app/api/v1/user";
+  "https://job-portal-backend-0tnh.onrender.com/api/v1/user";
 export const JOB_API_END_POINT =
-  "https://job-portal-rho-hazel.vercel.app/api/v1/job";
+  "https://job-portal-backend-0tnh.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "https://job-portal-rho-hazel.vercel.app/api/v1/application";
+  "https://job-portal-backend-0tnh.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT =
-  "https://job-portal-rho-hazel.vercel.app/api/v1/company";
+  "https://job-portal-backend-0tnh.onrender.com/api/v1/company";
