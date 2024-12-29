68f1808950656389c3dfa247a0f180dd7d5ab34f
pandey88himanshu <pandey88himanshu@gmail.com>
2024-12-29 15:53:29 +0530

    cors

--- src/components/utils/constant.js
+++ src/components/utils/constant.js
@@ -1,8 +1,8 @@
 export const USER_API_END_POINT =
-  "https://job-portal-backend-0tnh.onrender.com/api/v1/user";
+  "https://job-portal-backend-kd3p.onrender.com/api/v1/user";
 export const JOB_API_END_POINT =
-  "https://job-portal-backend-0tnh.onrender.com/api/v1/job";
+  "https://job-portal-backend-kd3p.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "https://job-portal-backend-0tnh.onrender.com/api/v1/application";
+  "https://job-portal-backend-kd3p.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT =
-  "https://job-portal-backend-0tnh.onrender.com/api/v1/company";
+  "https://job-portal-backend-kd3p.onrender.com/api/v1/company";
